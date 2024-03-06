f3f85a0f8d11bd78fd188c3603c7eb31bc8691f6
MokhammadVu <stechkin_95@hotmail.com>
2024-03-06 14:20:08 +0100

    up

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -22,7 +22,7 @@ export type Project = {
       title: "Car Rental Marketplace",
       description:
         "The MERN Stack Car Rental Marketplace is a dynamic web application designed to facilitate the seamless sharing of cars among users. This project showcases my proficiency in utilizing the MERN (MongoDB, Express.js, React, and Node.js) technology stack to create a user-friendly and efficient solution for car rental and sharing.",
-      image: "/projects/renoise.png",
+      image: "/projects/renoise1.png",
       tags: ["NEXT.JS", "CSS", "TYPESCRIPT", "HTML"],
       link: "http://link_to_car_rental"
     },
@@ -31,7 +31,7 @@ export type Project = {
         title: "ReNoise",
         description:
           "ReNoise is an interactive web application that I have designed to offer users an immersive experience with the soothing sounds of nature. This is a full-stack project, developed using modern web technologies and frameworks, ensuring a pleasant and intuitive user experience. In ReNoise, users have the capability to register and log into their accounts, providing a personalized experience.",
-        image: "/projects/renoise.png",
+        image: "/projects/renoise1.png",
         tags: ["NEXT.JS", "CSS", "TYPESCRIPT", "HTML"],
         link: "http://link_to_renoise"
       },
@@ -40,7 +40,7 @@ export type Project = {
         title: "Car Rental Marketplace",
         description:
           "The MERN Stack Car Rental Marketplace is a dynamic web application designed to facilitate the seamless sharing of cars among users. This project showcases my proficiency in utilizing the MERN (MongoDB, Express.js, React, and Node.js) technology stack to create a user-friendly and efficient solution for car rental and sharing.",
-        image: "/projects/renoise.png",
+        image: "/projects/renoise1.png",
         tags: ["NEXT.JS", "CSS", "TYPESCRIPT", "HTML"],
         link: "http://link_to_car_rental"
       },
